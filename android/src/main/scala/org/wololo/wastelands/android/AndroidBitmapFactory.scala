8ecdfbe2616dda1857e0ad1846ecabbf13285e52
Björn Harrtell <bjorn@wololo.org>
2012-01-10 22:28:51 +0100

    Ok implemented for android too :) (#18)

--- android/src/main/scala/org/wololo/wastelands/android/AndroidBitmapFactory.scala
+++ android/src/main/scala/org/wololo/wastelands/android/AndroidBitmapFactory.scala
@@ -3,6 +3,8 @@ import org.wololo.wastelands.vmlayer.BitmapFactory
 import android.graphics.Bitmap
 import java.io.InputStream
 import org.wololo.wastelands.vmlayer.BitmapTypes
+import android.graphics.Canvas
+import android.graphics.Color
 
 object AndroidBitmapFactory extends BitmapFactory[Bitmap] {
   def create(width: Int, height: Int, bitmapType: Int): Bitmap = {
@@ -11,12 +13,21 @@ object AndroidBitmapFactory extends BitmapFactory[Bitmap] {
   def create(inputStream: InputStream): Bitmap = {
     android.graphics.BitmapFactory.decodeStream(inputStream)
   }
-  def createShadow(bitmap: Bitmap) : Bitmap = {
-    // TODO: do proper, this is fake impl. will produce transparent image
-    Bitmap.createBitmap(bitmap.getWidth, bitmap.getHeight, Bitmap.Config.ARGB_8888)
+  def createShadow(bitmap: Bitmap): Bitmap = {
+    val shadow = Bitmap.createBitmap(bitmap.getWidth, bitmap.getHeight, Bitmap.Config.ARGB_8888)
+    new Canvas(shadow).drawColor(Color.argb(0, 0, 0, 0))
+
+    for (
+      y <- 0 until bitmap.getHeight;
+      x <- 0 until bitmap.getHeight
+    ) {
+      shadow.setPixel(x, y, (bitmap.getPixel(x, y) & 0x6f000000) | (shadow.getPixel(x, y) & 0x00ffffff))
+    }
+
+    shadow
   }
-  
-  private def parseBitmapType(bitmapType: Int) : Bitmap.Config = {
+
+  private def parseBitmapType(bitmapType: Int): Bitmap.Config = {
     bitmapType match {
       case BitmapTypes.Opague => Bitmap.Config.RGB_565
       case BitmapTypes.Bitmask => Bitmap.Config.ALPHA_8
