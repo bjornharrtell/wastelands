baf4f54269cb9790d137855500f0bfffaa430f51
Björn Harrtell <bjorn@wololo.org>
2012-01-18 23:21:46 +0100

    Use current volume for playback (#28)

--- android/src/main/scala/org/wololo/wastelands/android/AndroidSound.scala
+++ android/src/main/scala/org/wololo/wastelands/android/AndroidSound.scala
@@ -2,9 +2,14 @@ package org.wololo.wastelands.android
 import org.wololo.wastelands.vmlayer.Sound
 import android.media.MediaPlayer
 import android.media.SoundPool
+import android.media.AudioManager
 
-class AndroidSound(soundPool: SoundPool, id: Int) extends Sound {
+class AndroidSound(audioManager: AudioManager, soundPool: SoundPool, id: Int) extends Sound {
   def play() {
-    soundPool.play(id, 1, 1, 0, 0, 1)
+    val streamVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+    val streamMaxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+    val volume = streamVolume / streamMaxVolume
+
+    soundPool.play(id, volume, volume, 1, 0, 1)
   }
 }
--- android/src/main/scala/org/wololo/wastelands/android/AndroidSoundFactory.scala
+++ android/src/main/scala/org/wololo/wastelands/android/AndroidSoundFactory.scala
@@ -10,6 +10,7 @@ import android.media.SoundPool
 import android.media.AudioManager
 
 class AndroidSoundFactory(context: Context) extends SoundFactory {
+  val audioManager = context.getSystemService(Context.AUDIO_SERVICE).asInstanceOf[AudioManager]
   val soundPool = new SoundPool(20, AudioManager.STREAM_MUSIC, 0)
   
   def create(filename: String): Sound = {
@@ -20,6 +21,6 @@ class AndroidSoundFactory(context: Context) extends SoundFactory {
     val id = soundPool.load(fd, 1)
     fd.close
     
-    new AndroidSound(soundPool, id)
+    new AndroidSound(audioManager, soundPool, id)
   }
 }
